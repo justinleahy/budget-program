3402c52b74fb098b73d298e5085c9f0b9bf1b32c
Justin Leahy <justinjleahy@gmail.com>
2023-03-21 19:45:02 -0400

    Vue devtools is stuck

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavigationBar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar
   }
 }
 </script>
@@ -21,6 +20,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vh;
 }
 </style>
